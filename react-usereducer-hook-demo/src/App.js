490ad18fbdb69ffaf9bf9495413095fcb143eb75
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-04-03 15:26:20 +0530

    made the reducer function for the increment and decrement actions

--- react-usereducer-hook-demo/src/App.js
+++ react-usereducer-hook-demo/src/App.js
@@ -1,4 +1,21 @@
 import './App.css';
+import {useReducer} from 'react'
+
+const initialState = 0;
+
+const reducer = (state,action)=>{
+      switch (action.type) {
+        case "INCREMENT":
+          return state + 1;
+          break;
+        case "DECREMENT" :
+          return state - 1;
+          break;
+        default:
+          return state;
+          break;
+      }
+}
 
 function App() {
   return (
