ab69cceeaa77a091234d0aede401c1648e4e3021
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-04-03 16:04:05 +0530

    added the explanation for the useReducer hook

--- react-usereducer-hook-demo/src/App.js
+++ react-usereducer-hook-demo/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useReducer} from 'react'
+import {useReducer, useState} from 'react'
 
 const initialState = 0;
 
@@ -18,7 +18,14 @@ const reducer = (state,action)=>{
 }
 
 function App() {
+  //useReducer is similler to the useState hook 
+  //here below the useReducer hook takes a reducer function and initialState as args 
+  // and provides (by array destructuring) the mutable state variable and the dispatch function to mutate the state 
+  //the dispatch is same as the setState2 function of the useState hook
+  //the reducer here is the action making function which takes the prevState and the action type to distinguish between the 
+  // different operations while trigerring any specific action known to the reducer...
   const [state,dispatch]=useReducer(reducer,initialState);
+  const [state2,setState2]=useState(0);
   return (
     <div className="App">
       <h1>{state}</h1>
